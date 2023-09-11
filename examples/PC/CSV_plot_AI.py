b4040aff8ebead442194756e2b935e6516de62e1
chungleepeople <jinhong2009885@gmail.com>
2023-09-11 16:10:45 +0800

    update

--- examples/PC/CSV_plot_AI.py
+++ examples/PC/CSV_plot_AI.py
@@ -2,11 +2,14 @@
 from pandas import *
 import matplotlib.pyplot as plt
 
-# reading CSV file
-data = read_csv("desktop/AI_test.csv")
+## reading CSV file
+data = read_csv("AI_test.csv")
 
-# Convert  column data to list
+## Convert  column data to list
 ch1_data = data['ch1'].tolist()
 
+## Plot
+plt.xlabel('Points')
+plt.ylabel('Voltage(V)')
 plt.plot(ch1_data,  marker="o", markersize=3, markeredgecolor="red", markerfacecolor="green")
 plt.show()
