0778ca3348bb04badad08ac167c026eea1b987de
chunglee_people <45706135+chungleepeople@users.noreply.github.com>
2023-09-13 12:10:43 +0800

    v0.1.1 - Add pywpc_ex.py for SDcard pin initial (#17)
    v0.1.1 Date: 2023/09/13, Developer: Chunglee_people
    ----------------------------------------------------
    ### Added
    - pywpc_ex.py
    - v0.1.1_20230913.bin file
    
    ### Changed
    - example name:
        - Read from SDCard -> Read from SD Card
        - Write into SDCard -> Write to SD Card
    
    ### Fixed
    - mip.install.py's contents
    - Initialize sdcard pin in the class of SDcard

--- examples/PYCO_WIFI/SDCard/readTXT.py
+++ examples/PYCO_WIFI/SDCard/readTXT.py
@@ -1,35 +1,26 @@
 '''
-SDCard - Read from SDCard.py.
+SDCard - Read from SD Card.py.
 
 This example demonstrates how to read from txt file.
 
 Copyright (c) 2023 WPC Systems Ltd. All rights reserved.
 '''
 
-import sdcard
-from machine import Pin, SPI
-import os
+import pywpc_ex
 
 ## Initialize the SD card
-sd_spi = SPI(1, sck = Pin(14, Pin.OUT), mosi = Pin(13, Pin.OUT), miso = Pin(12, Pin.OUT))
-
-sd = sdcard.SDCard(sd_spi, Pin(15, Pin.OUT))
-
-## Create a instance of MicroPython Unix-like Virtual File System (VFS)
-vfs=os.VfsFat(sd)
-
-## Mount the SD card
-os.mount(sd,'/sd')
+dev = pywpc_ex.SDCard()
 
 ## Debug print SD card directory and files
-print(os.listdir('/sd'))
+print(dev.getFiles())
 
 ## Open the file in "read mode".
 ## Read the file and print the text on debug port.
-file = open("/sd/sample.txt", "r")
+file = dev.openFile("sample.txt","r")
+
 if file != 0:
     print("Reading from SD card")
-    read_data = file.read()
-    print (read_data)
+    print (dev.readFile())
+
 ## Close the file
-file.close()
+dev.closeFile()
--- examples/PYCO_WIFI/Tutorial_WIFI/mip_install.py
+++ examples/PYCO_WIFI/Tutorial_WIFI/mip_install.py
@@ -7,4 +7,4 @@ Copyright (c) 2023 WPC Systems Ltd. All rights reserved.
 '''
 
 import mip
-mip.install('github:olivergregorius/micropython_ota/micropython_ota.py')
+mip.install('sdcard.py')
