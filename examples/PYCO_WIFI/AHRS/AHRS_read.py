b9823788fa6e6d2807de99bf821de41c5350d00c
chungleepeople <jinhong2009885@gmail.com>
2023-12-07 14:24:12 +0800

    update

---
+++ examples/PYCO_WIFI/AHRS/AHRS_read.py
@@ -0,0 +1,24 @@
+'''
+AHRS - AHRS_read.py.
+
+This example demonstrates the process of obtaining AHRS three axis estimation data.
+
+Copyright (c) 2023 WPC Systems Ltd. All rights reserved.
+'''
+
+## WPC
+import pywpc
+
+## Parameters setting
+theo_grav = 9.81
+dt = 0.005
+offset_z = 0.005
+
+## Set general setting
+pywpc.AHRS_setGeneral(theo_grav, dt, offset_z)
+
+## AHRS start
+pywpc.AHRS_start()
+
+## Get AHRS three axis estimation
+print(pywpc.AHRS_getOrient())
