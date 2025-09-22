8669571f11aa94a95bb7949b9590380313ac9cf9
chunglee_people <45706135+chungleepeople@users.noreply.github.com>
2025-09-22 12:15:20 +0800

    v0.9.1.1 - Add Change IP & SSID 's example code (#65)
    v0.9.1 Date: 2025/09/22, Developer: Chunglee_people
    ---------------------------------------------------
    ### Added
    - Add example code: change_ip, not_execute_main_py, set_wifi_info

---
+++ examples/PYCO_WIFI_MINI_AO/System/change_ip.py
@@ -0,0 +1,20 @@
+
+'''
+System - scan_engine.py.
+
+For other examples please check:
+    https://github.com/WPC-Systems-Ltd/WPC_Stand-alone_Python_release/tree/main/examples
+
+Copyright (c) 2025 WPC Systems Ltd.
+All rights reserved.
+'''
+
+## WPC
+import pywpc
+
+## Set IP
+pywpc.Sys_setIP("your_IP_address")
+
+## Reboot PYCO
+pywpc.Sys_reboot()
+
